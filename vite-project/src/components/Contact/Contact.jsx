cf2ab967d0226b630867639d8fd57ae2b9f845f8
Sickcop <juan,hurtado2@ucp.edu.co>
2024-08-26 13:16:31 -0500

    front-not-fisnished

--- vite-project/src/components/Contact/Contact.jsx
+++ vite-project/src/components/Contact/Contact.jsx
@@ -7,9 +7,9 @@ function Contact() {
     <div className='sm:pt-20 sm:pb-20 m-auto'>
       <h1>Contactanos al instante</h1>
       <p className='mt-6 leading-8 text-gray-600 w-[32rem] m-auto text-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam architecto laboriosam assumenda, explicabo dolor sequi?</p>
-      <form>
-        <input type='email' className='bg-sky-50 h-10 rounded' />
-        <button>Enviar</button>
+      <form className='flex justify-center mt-10 gap-x-6'>
+        <input type='email' className='bg-sky-50 h-10 rounded p-5' placeholder='name@example.com' />
+        <button className=''>Enviar</button>
       </form>
     </div>
     </>
