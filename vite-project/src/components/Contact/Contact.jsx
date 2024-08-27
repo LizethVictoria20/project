b30d0e61657cb4dc7ccf0ed02dc7f4c25d769bd6
Sickcop <juan,hurtado2@ucp.edu.co>
2024-08-27 11:49:57 -0500

    only front

--- vite-project/src/components/Contact/Contact.jsx
+++ vite-project/src/components/Contact/Contact.jsx
@@ -5,9 +5,9 @@ import Whatsapp from './Whatsapp'
 
 function Contact() {
 
-  const [content, setContent] = useState(false)
+  const [content, setContent] = useState(true)
 
-  const switchContact = () => {
+  const switchContent = () => {
     setContent(!content)
   }
 
@@ -16,10 +16,11 @@ function Contact() {
     <div className='sm:pt-20 sm:pb-20 m-auto'>
       <h1>!!!Contactanos!!!</h1>
       <p className='mt-6 leading-8 text-gray-600 w-[32rem] m-auto text-2xl'>Si deseas saber más sobre nosotros no dudes en contactarnos para brindarte más información sobre nuestros servicios.</p>
-      <button>Email</button>
-      <button>Whatsapp</button>
-      <Email />
-      <Whatsapp />
+      <div className='flex justify-center mt-10 gap-x-6'>
+      <button onClick={switchContent}>Email</button>
+      <button onClick={switchContent}>Whatsapp</button>
+      </div>
+      {content ? <Email /> : <Whatsapp />}
     </div>
     </>
   )
